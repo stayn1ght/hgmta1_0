cf2148484962c8262b172d60f4c61a464b646fb2
stayn1ght <74388983+stayn1ght@users.noreply.github.com>
2023-06-21 16:36:16 +0800

    Update settings.py
    add static_root

--- hgmta1_0/settings.py
+++ hgmta1_0/settings.py
@@ -129,6 +129,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'collect_static/')
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
